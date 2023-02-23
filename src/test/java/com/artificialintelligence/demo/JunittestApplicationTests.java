6a92217dd0637f96b415323183bdc86e1b24ef2a
Nandan <Nandan1981@gmail.com>
2023-02-24 00:46:24 +0530

    initial commit

---
+++ src/test/java/com/artificialintelligence/demo/JunittestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artificialintelligence.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunittestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
